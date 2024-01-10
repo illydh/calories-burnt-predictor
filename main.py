13410c06779968cc28e41f9d2b6277821438e2f1
Illy Hoang <illyhoang@Illys-MacBook.local>
2024-01-10 17:06:56 -0500

    main script

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np          #   used to make arrays
+import pandas as pd         #   used to make dataframes / structured tables for the data   
+import matplotlib.pyplot as plt         #   used to create plots   
+import seaborn as sns       #   used to create graphs
+from sklearn.model_selection import train_test_split        #   used to split our data into training data and test data
+from xgboost import XGBRegressor        #   provides the gradient boosting algorithm
+from sklearn import metrics         #   used to evaluate our model
+
+
+
